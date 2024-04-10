d14957000eaca4cfe22061ea136e4bfec978e3a2
stiksteak <147105096+stiksteak@users.noreply.github.com>
2024-04-09 22:53:16 -0700

    feed

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,6 +1,6 @@
 var greeting = 'Hiya its ';
 var name = 'Darren';
-var message = ', im here to contact you about your cars extended warranty:';
+var message = ', im here to contact you about your cars extended warranty.';
 var welcomeMessage = greeting + name + message;
 
 var el = document.getElementById('greeting');
